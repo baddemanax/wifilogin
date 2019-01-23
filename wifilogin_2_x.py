ea4289397f157d57c6fb967874dd94aa964fec4e
baddemanax <baddemanax@hotmail.com>
2019-01-23 16:36:59 +0100

    Update wifilogin_2_x.py

--- wifilogin_2_x.py
+++ wifilogin_2_x.py
@@ -18,7 +18,7 @@ br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:13.0) Gecko/20100101 Firefox/13.0')]
 
-testURL = 'https://guest.cis.consilium.europa.eu/guest/portal.php'
+testURL = 'https://test.html'
 response = br.open(testURL)
 print ("response url : ")
 print response.read()
@@ -43,7 +43,7 @@ print form
 #print "----------------------------------- Login"
 request = form.click()
 response = mechanize.urlopen(request)
-print ("response to form : ")
+print ("response to first form : ")
 print response.read()
 #try:
 forms = mechanize.ParseResponse(response, backwards_compat=False)
@@ -58,7 +58,7 @@ sys.exit()
 form = forms[0]
 request = form.click()
 response = mechanize.urlopen(request)
-print ("response 2 to form : ")
+print ("response to second form : ")
 print response.read()
 try:
 	forms = mechanize.ParseResponse(response, backwards_compat=False)
@@ -68,4 +68,4 @@ except:
 	print (" error while opening last form ")
 	response.close()
 response.close()
-print "--- council eu Done ---"
+print "--- login  Done ---"
